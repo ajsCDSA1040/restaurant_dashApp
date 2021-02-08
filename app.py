0b49bc91d0d29bb3f86d0da2f6d5d1a2bdf70a71
ajsCDSA1040 <78503856+ajsCDSA1040@users.noreply.github.com>
2021-02-08 17:25:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def get_restaurant_city(index):
 #Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 colors = {
     'background': '#111111',
